57869e9da1bc8c7da4e50f8b75aa6a3cf57e4b3d
DAKudryashev <kd_291@mail.ru>
2025-03-19 11:42:18 +0300

    add class parser with methods get schedule_by_days, normalize_text

--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,3 @@
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
@@ -6,28 +5,63 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get("https://mai.ru/education/studies/schedule/index.php?group=М3О-312Б-22")
+from bs4 import BeautifulSoup
+import string
 
-element = driver.find_element(By.ID,  'department')
-# print(element.is_displayed(), element.is_enabled())
-select = Select(element)
-select.select_by_value('Институт №3')
 
-element1 = driver.find_element(By.ID,  'course')
-# print(element1.is_displayed(), element1.is_enabled())
-select1 = Select(element1)
-select1.select_by_value('3')
+class Parser:
+    def __init__(self, department, course, group):
+        self.department = department
+        self.course = course
+        self.group = group
+        self.text_by_days = []
 
-driver.find_element(By.CSS_SELECTOR, 'div.col-md-3.col-5.mb-2.mb-md-0').click()
-groups = driver.find_elements(By.CSS_SELECTOR, 'a.btn.btn-soft-secondary.btn-xs.mb-1.fw-medium.btn-group')
-# Явное ожидание, чтобы дождаться загрузки кнопки
-wait = WebDriverWait(driver, 10)
-button = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, 'М3О-312Б-22')))
-driver.execute_script("arguments[0].click();", button)
+    def get_schedule_by_days(self):
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver.get("https://mai.ru/education/studies/schedule/index.php?group=М3О-312Б-22")
 
-soup = BeautifulSoup(driver.page_source, 'html.parser')
+        # Выбор института
+        element = driver.find_element(By.ID, 'department')
+        # print(element.is_displayed(), element.is_enabled())
+        select = Select(element)
+        select.select_by_value('Институт №3')
 
-temp = soup.find_all('div', 'step-content-wrapper')
-print(temp[0].text)
-driver.close()
+        # Выбор курса и переход на следующую страницу
+        element1 = driver.find_element(By.ID, 'course')
+        # print(element1.is_displayed(), element1.is_enabled())
+        select1 = Select(element1)
+        select1.select_by_value('3')
+        driver.find_element(By.CSS_SELECTOR, 'div.col-md-3.col-5.mb-2.mb-md-0').click()
+
+        # Ожидание загрузки кнопки и выбор группы
+        wait = WebDriverWait(driver, 10)
+        button = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, 'М3О-312Б-22')))
+        driver.execute_script("arguments[0].click();", button)
+
+        # Парсинг страницы с открывшимся расписанием
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        if self.group not in soup.find('h1', 'mb-5').text:
+            print('incorrect open of group page')
+        else:
+            days = soup.find_all('div', 'step-content-wrapper')
+            self.text_by_days = [i.text for i in days]
+            # print(self.text_by_days)
+            driver.close()
+
+    def normalize_text(self):
+        for i in range(len(self.text_by_days)):
+            tmp = self.text_by_days[i].replace('\xa0', ' ')
+            tmp = tmp.split('\t')
+            while '' in tmp:
+                tmp.remove('')
+            tmp = ' '.join(tmp)
+            tmp = tmp.replace('\n', '')
+            tmp = tmp.replace('  ', ' ')
+            print(tmp)
+            self.text_by_days[i] = tmp
+
+
+if __name__ == '__main__':
+    parser_a = Parser('Институт №3', '3', 'М3О-312Б-22')
+    parser_a.get_schedule_by_days()
+    parser_a.normalize_text()
